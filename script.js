ed808ae2e39662537e59ae6aafc33136d5a9d53c
McHammmmmm <136137425+McHammmmmm@users.noreply.github.com>
2025-02-13 01:53:14 -0800

    Update script.js

--- script.js
+++ script.js
@@ -91,7 +91,6 @@ function displaycmM() {
 }
 
 function displayMessage() {
-    textBox = new te
     document.getElementById('no-button').innerText = 'Hey Catherine, my 0.5 valentine, happy valentines day! All my feelings are in the letters that I wrote you, and they have only intensified since I got back. I think about our time in Shanghai everyday, and I miss it dearly. I only wish to have you here by my side, so that I can hold you in my arms and kiss you, and to tell you all my love for you. BTW, would you happen to be willing to go on an adventure with me to Guam? :)'; 
 }
 
